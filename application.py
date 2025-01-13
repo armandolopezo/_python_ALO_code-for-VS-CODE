bd90a0d44b889230d953a54e5e7674f1e9a9fc63
armandolopezo <armandolopezo@hotmail.com>
2025-01-13 13:50:35 -0400

    copy web.py a application.py

--- application.py
+++ application.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
+@app.route('/')
+def page():
+    return redirect('http://mediatech.com.ec/', code=301)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
 
-@app.route("/")
-def hello():
-    return "<html><body><h1>Hello Best Bike App!</h1></body></html>\n"
+  
