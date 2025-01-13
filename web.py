fb77a5fe028ded0bad9fb10cdebb3b48cfb769d1
armandolopezo <armandolopezo@hotmail.com>
2025-01-13 13:20:34 -0400

    version de programa web para redirigir web page a mediatech con Azure App Service

--- web.py
+++ web.py
@@ -1,11 +1,8 @@
 from flask import Flask, redirect
-
 app = Flask(__name__)
-
 @app.route('/')
 def page():
     return redirect('http://mediatech.com.ec/', code=301)
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
 
