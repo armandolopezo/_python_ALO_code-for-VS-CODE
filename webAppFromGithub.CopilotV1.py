29eed432610876666c360ad8184a2dd049872231
armandolopezo <armandolopezo@hotmail.com>
2025-01-25 10:45:41 -0400

    prueba 1 de webapp con python y flash consultando a github copilot

---
+++ webAppFromGithub.CopilotV1.py
@@ -0,0 +1,12 @@
+from flask import Flask, redirect
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def page():
+    return redirect('http://mediatech.com.ec/', code=301)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 80))
+    app.run(host='0.0.0.0', port=port)
